c2b2204c40ad46fce6cd02f3a9019385ecd8eced
MarwaAshraf1812 <marashraf090@gmail.com>
2024-05-30 19:05:29 +0300

    updated

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -21,48 +21,41 @@ if N < 4:
 
 
 def solveNQueens(n):
-    """Solution for n queens"""
+    '''
+    Solve the N queens problem
+    '''
     col = set()
     pos = set()
     neg = set()
 
-    final_results = []
-
-    board = [[] for n in range(n)]  # create empy board
+    final_result = []
+    board = [[] for n in range(n)]
 
     def solve(row):
-        """function for recursion"""
-        # means we've reached the last row
+        '''recursive function to solve the n queens problem'''
         if row == n:
-            # get copy of current solution(current board)
-            final_results.append(board.copy())
+            '''base case'''
+            final_result.append(board.copy())
             return
-
-        # for every column
         for i in range(n):
-            # if we find that the column or diagonals are used, then skip
             if i in col or (row + i) in pos or (row - i) in neg:
                 continue
 
-            # register found columns and diagonals
             col.add(i)
             pos.add(row + i)
             neg.add(row - i)
 
             board[row] = [row, i]
 
-            # move to next row
             solve(row + 1)
 
-            # finally undo
             col.remove(i)
             pos.remove(row + i)
             neg.remove(row - i)
             board[row] = []
 
     solve(0)
-
-    return final_results
+    return final_result
 
 
 if __name__ == '__main__':
